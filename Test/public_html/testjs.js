2cc44c086ffedd47056319a7bb920ef03a2f4851
Meaxt <Arnestad96@hotmail.com>
2015-04-21 13:56:43 +0200

    hej
    går frammåt

--- Test/public_html/testjs.js
+++ Test/public_html/testjs.js
@@ -3,15 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-          myFirebaseRef.set({
-             tittle:"hello world!",
-             author:"Firebase",
-             location:{
-                 city:"lkpg",
-                 
-             }
-          });
-          myFirebaseRef.child("location/city").on("value", function(snapshot){
-             alert(snapshot.val()); 
-          });
+console.log("hej");
+var ref = new Firebase('https://radiant-heat-57.firebaseio.com/')
+//var usersRef = ref.child("users");
+//usersRef.set({
+//    alanisawesome: {
+//        date_of_birth: "June 23, 1912",
+//        full_name: "Alan Turing"
+//    },
+//    gracehop: {
+//        date_of_birth: "December 9, 1906",
+//        full_name: "Grace Hopper"
+//    }
+//});
+//var postsRef = ref.child("posts");
+//  postsRef.push({
+//    author: "gracehop",
+//    title: "Announcing COBOL, a New Programming Language"
+//  });
+//  postsRef.push({
+//    author: "alanisawesome",
+//    title: "The Turing Machine"
+//  });
+//  ref.on("value", function(snapshot) {
+//  console.log(snapshot.val());
+//}, function (errorObject) {
+//  console.log("The read failed: " + errorObject.code);
+//});
 
+//$('#messageInput').keypress(function (e) {
+//        if (e.keyCode == 13) {
+//          var name = $('#nameInput').val();
+//          var text = $('#messageInput').val();
+//          ref.push('User ' + name + ' says ' + text);
+//          $('#messageInput').val('');
+//        }
+//      });
